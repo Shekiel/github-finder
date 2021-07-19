f03a0074ca231cb44e3731dd286fb9c7975e9dd1
shekiel <shekielmorris1@gmail.com>
2021-07-19 22:53:58 +0100

    create components folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React, { Component } from 'react';
+import Navbar from './componets/layout/Navbar';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello from react</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar title="Github-Finder" />
+      </div>
+    );
+  }
 }
 
 export default App;

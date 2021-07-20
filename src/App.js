700260d42affa9eac6ef237e0e28582255bac15a
shekiel <shekielmorris1@gmail.com>
2021-07-20 15:21:07 +0100

    build UserItem component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Navbar from './componets/layout/Navbar';
+import UserItem from './componets/users/UserItem';
 import './App.css';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <UserItem />
       </div>
     );
   }

002440a988642585cafa5277c73df434db4fc9a9
shekiel <shekielmorris1@gmail.com>
2021-07-21 15:47:57 +0100

    create clear button

--- src/componets/users/Search.js
+++ src/componets/users/Search.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Search extends Component {
   state = {
     text: '',
   };
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+  };
+
   onSubmit = (e) => {
     e.preventDefault();
     this.props.searchUsers(this.state.text);
@@ -29,6 +37,14 @@ export class Search extends Component {
             className="btn btn-dark btn-block"
           />
         </form>
+        {this.props.showClear && (
+          <button
+            className="btn btn-light btn-block"
+            onClick={this.props.clearUsers}
+          >
+            Clear
+          </button>
+        )}
       </div>
     );
   }

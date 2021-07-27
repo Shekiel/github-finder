5bbf353abc96cac5bb5b733eece724f7e79f4c86
shekiel <shekielmorris1@gmail.com>
2021-07-27 22:56:25 +0100

    create Home & NotFound Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './componets/layout/Navbar';
-import Users from './componets/users/Users';
-import Search from './componets/users/Search';
-import Alert from './componets/layout/Alert';
-import About from './componets/pages/About';
 import User from './componets/users/User';
-
+import Alert from './componets/layout/Alert';
+import Home from './componets/pages/Home';
+import About from './componets/pages/About';
+import NotFound from './componets/pages/NotFound';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
 import './App.css';
@@ -20,18 +19,10 @@ const App = () => {
             <div className="container">
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path="/"
-                  render={(props) => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/user/:login" component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
